0353cdb853d4e636915fd353390c2f9311047e0d
James Fm <fmj2933@gmail.com>
2024-01-31 21:12:04 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/interactivity_and_theming_05/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.interactivity_and_theming_05
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:interactivity_and_theming_05/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
